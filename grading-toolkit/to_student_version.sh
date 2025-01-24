c0ff8a5bace69df6106d172afb2e7c3c67b46f68
Sadamori Kojaku <skojaku@iu.edu>
2025-01-24 16:46:08 -0500

    update

--- grading-toolkit/to_student_version.sh
+++ grading-toolkit/to_student_version.sh
@@ -6,12 +6,20 @@ fi
 teacher_notebook=$1
 student_notebook=$2
 encryption_password=$3
+tmp_notebook="assignment.ipynb"
 
 teacher_notebook_enc=${teacher_notebook%.ipynb}.ipynb.enc
 
 openssl enc -aes256 -in $teacher_notebook -out $teacher_notebook_enc -pass pass:$encryption_password
 
+cp $teacher_notebook $tmp_notebook
+
 jupyter nbconvert --to notebook --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='["answer"]' $teacher_notebook --inplace
+
+# Then use jq to rename "answer" to "gradable"
+jupyter nbconvert --to notebook --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags='["gradable"]' $tmp_notebook --inplace
+jq '.cells[].metadata.tags |= if . != null then map(if . == "answer" then "gradable" else . end) else . end' $tmp_notebook -i
+
 
 mv $teacher_notebook $student_notebook
 
